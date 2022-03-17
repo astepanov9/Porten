40e0d4a21ba03b17d93deb7e10fef2626f262525
astepanov9 <mileez@yandex.ru>
2022-03-18 00:40:36 +0300

    Добавлено: меню бургер, кнопка наверх, плавная прокрутка, поп-ап окно

---
+++ js/script.js
@@ -0,0 +1,62 @@
+$(function(){
+	
+// Кнопка наверх
+$(window).scroll(function(){
+    if($(window).scrollTop() > 100) {
+        $('.button-up').show();
+    } else {
+        $('.button-up').hide();
+    }
+});
+
+$('.button-up').click(function(){
+    $('html, body').animate({scrollTop: 0}, 600);
+    return false;
+});
+
+// Меню гамбургер
+$('.nav-menu__toggle').click(function (e){
+    e.preventDefault();
+    $('.menu-header__nav').toggleClass('show');
+});
+
+if ($(window).width() < 768) {
+    $('.menu-header__nav li a').click(function(e) {
+        e.preventDefault();
+        $('.menu-header__nav').toggleClass('show');
+    });
+};
+
+// Плавная прокрутка
+$("a.scroll-to").on("click", function(e){
+    e.preventDefault();
+    let elementClick = $(this).attr('href');
+    $('html, body').stop().animate({
+        scrollTop: $(elementClick).offset().top - 60
+    }, 1000);
+});
+
+
+// Попап окно (прописать кнопке href="#modal" class="modal-btn")
+$('a.modal-btn').click(function (e) {
+    e.preventDefault();
+    let destinationPopup = $(this).attr('href');
+    $(destinationPopup).addClass('show');
+    $(destinationPopup + ' .popup__close').click(function (e) {
+        e.preventDefault();
+        $(destinationPopup).removeClass('show');
+    });
+    $(destinationPopup + ' .popup__overlay').click(function (e) {
+        e.preventDefault();
+        $(destinationPopup).removeClass('show');
+    });
+});
+
+// Слайдер
+
+
+// 3D-эффекты
+
+
+
+});
